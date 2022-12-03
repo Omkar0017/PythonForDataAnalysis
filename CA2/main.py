a0d391a4d9f52dc789362ceb23dc32b80f1055bf
Omkar <omkarp0017@gmail.com>
2022-12-03 18:52:11 +0000

    final commit for main

--- CA2/main.py
+++ CA2/main.py
@@ -1,17 +1,37 @@
 from WebScrappingAmazon import ScrapAmazon
 from WebScrappingCroma import ScrapCroma
 from WebScrappingFlipkart import ScrapFlipkart
-import time
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
 Fp = ScrapFlipkart()
 Az = ScrapAmazon()
 Cr = ScrapCroma()
+template = "An exception of type {0} occurred."
 
-df1 = Fp.get_data()
-df2 = Az.getData()
-df3 = Cr.get_data()
+df1 = pd.DataFrame()
+
+"""
+df2 = pd.DataFrame()
+df3 = pd.DataFrame()
+
+
+try:
+    df1 = Fp.get_data()
+except  Exception as ex :
+    print(template.format(type(ex).__name__))
+    
+try:  
+    df2 = Az.getData()
+except  Exception as ex :
+    print(template.format(type(ex).__name__))
+
+try:
+    df3 = Cr.get_data()
+except  Exception as ex :
+    print(template.format(type(ex).__name__))
 
 
 print('**********************************')
@@ -30,18 +50,87 @@ print('Croma')
 
 print(df3.size)
 #print(df3.head())
-#print(df3.info())
-#s = int(input("Please Select Which iPhone you would like to select \n 1: iPhone 12 \n 2: iPhone 13 \n 3: iPhone 14"))
+print(df1.info())
+s = int(input("Please Select Which iPhone you would like to select \n 1: iPhone 12 \n 2: iPhone 13 \n 3: iPhone 14"))
 s = 2
 if s == 1:
     name = 'Apple\iPhone\12'
 elif s ==2:
-    name = 'Apple\iPhone\13'
+    #name = 'Apple\tiPhone\t13'
+    name = '13'
 elif s == 3:
     name = 'Apple\iPhone\14'
 else:
     print("Invalid input")
 
 print(df1.info())   
+print('productList.str.contains({0})'.format(name))
 fkDf = df1.query('productList.str.contains({0})'.format(name))
-print(fkDf.head())
+print(fkDf.head())"""
+
+
+
+
+status = 'Y'
+while status == 'Y':
+    print("Please Select Which Website you would like to select \n 1 for Flipkart.com  \n 2 for Amazon.in  \n 3 for Croma.com")
+    site = int(input("Input: "))
+
+    if site == 1:
+        name = 'Flipkart'
+        try:
+            df1 = Fp.get_data()
+            finalDf = df1.sort_values('Price',ascending= True).head(20)
+
+            plt.figure(figsize=(10,10))
+            plt.barh(finalDf['productList'],finalDf['Price'])
+            plt.xlabel('Name')
+            plt.ylabel('Price')
+            plt.grid()
+            plt.title('Price vs Product for Flipkart')
+            plt.show()
+
+        except  Exception as ex :
+            print(template.format(type(ex).__name__))
+
+
+    elif site == 2:
+        name = 'Amazon'
+        try:
+            df1 = Az.getData()
+            finalDf = df1.sort_values('Price',ascending= True).head(20)
+
+            plt.figure(figsize=(10,10))
+            plt.barh(finalDf['productList'],finalDf['Price'])
+            plt.xlabel('Name')
+            plt.ylabel('Price')
+            plt.grid()
+            plt.title('Price vs Product for Amazon')
+            plt.show()
+        except  Exception as ex :
+            print(template.format(type(ex).__name__))
+
+    elif site == 3:
+        name = 'Croma'
+        try:
+            df1 = Cr.get_data()
+            finalDf = df1.sort_values('Price',ascending= True).head(20)
+     
+            plt.figure(figsize=(10,10))
+            plt.barh(finalDf['productList'],finalDf['Price'])
+            plt.xlabel('Name')
+            plt.ylabel('Price')
+            plt.grid()
+            plt.title('Price vs Product for Croma')
+            plt.show()
+        except  Exception as ex :
+            print(template.format(type(ex).__name__))
+        
+        
+    else:
+        print("Invalid input")
+        
+
+    status = input('Do you want to try Again  Y/N ?').capitalize()
+    print(status)
+
